653bf6cb8c7f80111264c8adbb064d5556b8fa14
Elise Erickson <elisece717@gmail.com>
2024-01-17 18:01:13 -0500

    creates and satisfies palindrome tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,22 @@
 // Your tests here
+import { isPalindrome } from "../utils"
+
+describe("isPalindrome", () => {
+    test("tests if word is the same front to back", () => {
+        const word = ""
+        const bool = isPalindrome(word)
+        expect(bool).toBe(false)
+    })
+
+    test("tests if word is the same front to back", () => {
+        const word = "raCecaR"
+        const bool = isPalindrome(word)
+        expect(bool).toBe(true)
+    })
+
+    test("tests if word is the same front to back", () => {
+        const word = "raCecaR532"
+        const bool = isPalindrome(word)
+        expect(bool).toBe(false)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,10 @@
 // Your code here
+export function isPalindrome(word){
+    let bool = word.length > 0
+    word.split("").forEach((letter, i) => {
+        if(i < word.length / 2 && letter.toLowerCase() != word.charAt(word.length - i - 1).toLowerCase()){
+            bool = false
+        }
+    })
+    return bool
+}
